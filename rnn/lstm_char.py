5c2f42f53273030abab41477463448a43996f831
thegithubgeek <wfrankw9@gmail.com>
2017-11-24 17:33:07 -0800

    work on lstm_char.py

--- rnn/lstm_char.py
+++ rnn/lstm_char.py
@@ -1,9 +1,11 @@
 import tensorflow as tf
+from tensorflow.contrib import rnn
 import numpy as np
 
 hidden_size = 50
 out_size = 128
 epochs = 1000
+num_layers = 3
 
 
 def process_data(text):
@@ -14,8 +16,9 @@ def process_data(text):
     for i in range(len(f_y)):
         f_y[i][arr[i + 1]] = 1
     return [f_x, f_y]
-
-with open("rnn/text.txt") as f:
+# with floydhub
+# with open("rnn/text.txt") as f:
+with open("text.txt") as f:
     data = " ".join(f.readlines())
 
 train_x, train_y = process_data(data)
@@ -24,24 +27,26 @@ seq_len = len(train_x)
 
 
 print("Making graph")
-x = tf.placeholder("float", [seq_len, 1])
-y = tf.placeholder("float", [seq_len, out_size])
+x = tf.placeholder("float", [None, seq_len, 1])
+y = tf.placeholder("float", [None, seq_len, out_size])
 x = tf.nn.l2_normalize(x, 0)
 
-lstm = tf.contrib.rnn.core_rnn_cell.BasicLSTMCell(hidden_size)
+layers = []
+for l in range(num_layers):
+    layers.append(rnn.BasicLSTMCell(hidden_size))
+cell = rnn.MultiRNNCell(layers)
 
-hidden_state = tf.zeros([seq_len, hidden_size])
-current_state = tf.zeros([seq_len, hidden_size])
-state = hidden_state, current_state
+x = tf.unstack(x, seq_len, 1)
+output, current_state = rnn.static_rnn(cell, x, dtype=tf.float32)
+outputs = tf.transpose(output, [1, 0, 2])
 
 w = tf.Variable(tf.truncated_normal([hidden_size, out_size], stddev=0.01))
 b = tf.Variable(tf.truncated_normal([out_size], stddev=0.01))
 
-output, state = lstm(x, state)
-out = tf.matmul(output, w) + b
+out = [tf.matmul(output, w) + b for output in outputs]
 loss = tf.reduce_sum(tf.losses.softmax_cross_entropy(y, out))
 
-train = tf.train.AdamOptimizer(0.01).minimize(loss)
+train = tf.train.AdagradOptimizer(0.3).minimize(loss)
 init = tf.global_variables_initializer()
 
 
